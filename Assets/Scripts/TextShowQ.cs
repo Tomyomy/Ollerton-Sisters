92f8ee0a626acfb3f921ada18b5e6d5bfbfa3133
Tomyomy <thomascharlesturner@gmail.com>
2018-04-18 10:21:12 +0100

    no u

---
+++ Assets/Scripts/TextShowQ.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+public class TextShowQ : MonoBehaviour
+{
+
+    bool crossedBoundary;
+    int DelayTime = 0;
+    int UnixTime()
+    {
+        return (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            crossedBoundary = true;
+        }
+    }
+    void OnGUI()
+    {
+
+        if (crossedBoundary == true)
+        {
+            GUI.Label(new Rect(650, 800, 600, 200), "Hello! I bet you're wondering what is going on here...hehe... but I can assure that you will understand everything in due time...");
+            if (DelayTime == 0) { DelayTime = UnixTime() + 10; }
+            if (DelayTime == UnixTime())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
